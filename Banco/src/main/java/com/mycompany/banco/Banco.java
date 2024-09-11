7622be7910b9733f2c0dae891e541ed476021d41
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-09-10 22:36:57 -0300

    ModelagemMVC

---
+++ Banco/src/main/java/com/mycompany/banco/Banco.java
@@ -0,0 +1,13 @@
+package com.mycompany.banco;
+import model.Usuario;
+import model.Endereco;
+import controller.UsuarioControle;
+import view.ViewUsuario;
+
+public class Banco {
+
+    public static void main(String[] args) {
+        ViewUsuario tela = new ViewUsuario();
+        tela.setVisible(true);
+    }
+}
---
+++ Banco/src/main/java/model/Gerente.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Gerente {
+    int codigoGerente;
+    
+    public int getCodigo(){
+        return codigoGerente;
+    }
+    public void setCodigo(int codigoGerente){
+        this.codigoGerente = codigoGerente;
+    }
+}
